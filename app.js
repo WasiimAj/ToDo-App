9b66925225eef29f3e72ebe42a23154b853160e1
WasiimAj <ajramewasem@gmail.com>
2022-12-09 14:20:36 +0200

    todo app upload

--- app.js
+++ app.js
@@ -1,31 +1,74 @@
-var timer = document.getElementById('timer');
-var start = document.getElementById('start');
-var reset = document.getElementById('reset');
+var closeBtn = document.getElementById('closeBtn');
+var addBtn = document.getElementById('addBtn');
+var createNew = document.getElementById('createNew');
+var addForm = document.getElementById('addForm');
+var todoTitle = document.getElementById('todoTitle');
+var todoDiscription = document.getElementById('todoDiscription');
+var todosList = document.getElementById('todos-list');
 
-var setTimer = document.getElementById('set-timer')
+createNew.addEventListener('click', () => {
+    addForm.classList.remove('hide');
+    createNew.classList.add('hide')
+    closeBtn.classList.remove('hide')
+    addBtn.classList.remove('hide')
+})
 
-var defaultTimer = 5;
-var time = defaultTimer * 60;
+closeBtn.addEventListener('click', () => {
+    addForm.classList.add('hide');
+    createNew.classList.remove('hide')
+    closeBtn.classList.add('hide')
+    addBtn.classList.add('hide')
+})
 
-setInterval(Timer, 1000);
-
-function Timer ()  {
-        let minutes = Math.floor(time /60);
-        let seconds = time % 60;
-        timer.innerHTML = `${minutes}m: ${seconds}s`;
-        if (time >= 1 ) {
-            time--;
-        } else {
-            time = 0;
-            timer.style.color = 'red';
+addBtn.addEventListener('click', () => {
+    fetch(
+        'https://jsonplaceholder.typicode.com/comments',
+        {
+            title: todoTitle.value,
+            description: todoDiscription.value
         }
+    )
+    .then(res => {
+        res.json()
+        console.log('status',res.status)
+        console.log(res)
+    })
+    .then(() => {
+        console.log('Added successfully ..')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    // console.log(todoTitle.value)
+    // console.log(todoDiscription.value)
+})
+
+        
+const callApi = (method, url) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                resolve(JSON.parse(xhr.response))
+            }}
+        xhr.open(method, url);
+        xhr.send();
+    });
 }
-
-start.addEventListener('click', () => {
-        time = setTimer.value * 60;
-})
-
-reset.addEventListener('click', () => {
-    time = 5 * 60;
-    timer.innerHTML = `${5}: ${00}`;
-})
+callApi("GET", 'https://jsonplaceholder.typicode.com/comments')
+    .then(res => 
+        // console.log(res)
+        res.map(i => {
+            var box = document.createElement('div');
+            box.className = 'todo-card'
+            var title = document.createElement('h1');
+            title.className = 'title';
+            title.innerHTML = i.name;
+            var body = document.createElement('p');
+            body.className = 'body';
+            body.innerHTML = i.body;
+            box.appendChild(title);
+            box.appendChild(body);
+            todosList.appendChild(box);
+        })
+    );
